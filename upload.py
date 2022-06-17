7cf4fc200eb5671c76a478b327bfb144fc246895
bending456 <bending456@gmail.com>
2022-06-17 09:21:27 -0400

    blah

--- upload.py
+++ upload.py
@@ -5,12 +5,12 @@ def main():
 
 	menu = ["Image","Dataset","DocumentFiles","About"]
 	choice = st.sidebar.selectbox("Menu",menu)
-
+	
 	if choice == "Image":
 		st.subheader("Image")
 
-    elif choice == "Dataset":
+	elif choice == "Dataset":
 		st.subheader("Dataset")
 
-    elif choice == "DocumentFiles":
+	elif choice == "DocumentFiles":
 		st.subheader("DocumentFiles")
